f38df6ad57d525fa545e26cd27547beba524ed0f
Usha-medhe <ushamedhe1993@gmail.com>
2023-03-09 16:42:46 +0530

    show msgs

---
+++ src/Messages/MessagesShow.java
@@ -0,0 +1,12 @@
+package Messages;
+
+public class MessagesShow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to India");
+		System.out.println("hello everyone");
+
+	}
+
+}
